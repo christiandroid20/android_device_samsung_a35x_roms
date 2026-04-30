47488daa498af82e3f39782b159bc04584e12395
Christy Vela <75439616+christiandroid20@users.noreply.github.com>
2026-04-30 13:16:54 -0600

    Update lineage_a35x.mk

--- lineage_a35x.mk
+++ lineage_a35x.mk
@@ -1,28 +1,54 @@
 #
-# Copyright (C) 2024 The LineageOS Project
-#
+# Copyright (C) 2026
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from core product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from full base
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit from a35x device
+# Inherit from device configuration
 $(call inherit-product, device/samsung/a35x/device.mk)
 
+# Inherit from vendor blobs
+$(call inherit-product, vendor/samsung/a35x/a35x-vendor.mk)
+
+# Product identity
 PRODUCT_DEVICE := a35x
 PRODUCT_NAME := lineage_a35x
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A356B
+PRODUCT_MODEL := SM-A356E
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
+# Build fingerprint (puedes ajustar luego)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a35xxx-user 14 UP1A.231005.007 A356BXXS3AXI3 release-keys"
+    PRIVATE_BUILD_DESC="a35x-user 15 AP3A.241105.008 release-keys"
 
-BUILD_FINGERPRINT := samsung/a35xxx/a35x:14/UP1A.231005.007/A356BXXS3AXI3:user/release-keys
+BUILD_FINGERPRINT := samsung/a35x/a35x:15/AP3A.241105.008:user/release-keys
+
+# LineageOS specific flags
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Enable A/B (si aplica, si no, comenta esto)
+# AB_OTA_UPDATER := true
+
+# Boot animation (opcional)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Shipping API level (ajusta si sabes el correcto)
+PRODUCT_SHIPPING_API_LEVEL := 34
+
+# Include common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Include extra packages (puedes agregar más luego)
+PRODUCT_PACKAGES += \
+    adb_root \
+    bash \
+    curl \
+    vim
+
+# Debugging
+WITH_DEXPREOPT := false
